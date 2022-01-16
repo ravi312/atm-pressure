0e756b643a021af2346d762372766eb93991736f
ravi312 <ravisinha312@gmail.com>
2022-01-16 12:24:37 +0530

    initial commit

---
+++ s3.js
@@ -0,0 +1,74 @@
+var sliderH;
+var sliderG;
+var sliderT;
+var sliderP;
+
+var R=287.053;
+
+var ho=8830;
+var go=9.8;
+var to=288;
+var po=101.325;
+
+var p;
+var hx;
+var py;
+
+function setup() {
+
+  createCanvas(400, 400);
+  sliderH=createSlider(0,30000,8830,1);
+  sliderG=createSlider(3,15,9.8,0.1);
+  sliderT=createSlider(248,320,288,1);
+  sliderP=createSlider(0,200,101.325,0.1);
+
+}
+
+function draw() {
+  background(51,28);
+  ho=sliderH.value();
+  go=sliderG.value();
+  to=sliderT.value();
+  po=sliderP.value();
+  sliderH.position(225, 310);
+  sliderG.position(225, 330);
+  sliderT.position(225, 350);
+  sliderP.position(225, 370);
+  translate(width/2-100,height/2+100);
+  stroke(255);
+  textSize(10);
+  stroke(255);
+  strokeWeight(2 );
+  p=po*Math.exp(-go*ho/(R*to));
+  noStroke();
+  textSize(12);
+  strokeWeight(2);
+  text('Height(m): '+ round(ho,1), 30, 20);
+  text('g: '+ round(go,2), 30, 40);
+  text('T: '+ round(to,2), 30, 60);
+  text('Po: '+ round(po,1), 30, 80);
+  text('Pressure (kPa) ', -90, -140);
+  text(round(p,1), -60, -120);
+ hx=map(ho,0,30000,0,10); 
+ py= map (p,0,200,0,10);
+ stroke(255,100);
+ point(25*hx,-25*py);
+ line(0, 0, 250, 0);
+ stroke(255);
+ line(0, 0, 0, -250);
+ stroke(255);
+ 
+ for(var j=0;j>-250;j-=25){
+  line(1, j, -1, j);
+}
+
+for(var j=0;j>-250;j-=41.6){
+  line(-j, 1, -j, -1);
+}
+
+
+fill(0,200,200,200);
+noStroke();
+rect(-10, -(25*py), 10, 2);
+
+}
